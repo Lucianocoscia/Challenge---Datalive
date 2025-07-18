0baf88603aac268ec7498c9ad4f7bb8abf1d581a
Luciano <lucianocoscia5@gmail.com>
2025-07-18 06:54:38 -0300

    Detalles entrega

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,11 @@ app.get("/api/cryptos", async(req, res) => {
                 'Content-Type': 'application/json',
             }
         });
-        console.log("STATUS:", response.status) // Vemos el estado de la respuesta
+        console.log("STATUS:", response.status); // Vemos el estado de la respuesta
         const data = await response.json();
         res.json(data);
     } catch (error) {
-        console.error("Error al obtener datos del listado normal:", error)
+        console.error("Error al obtener datos del listado normal:", error);
         res.status(500).json({error: "Error al obtener datos"});
     }
 });
@@ -37,11 +37,11 @@ app.get("/api/cryptos/categorias", async(req, res) => {
                 'Content-Type': 'application/json',
             }
         });
-        console.log("STATUS de la ruta categorias:", response.status) // Vemos el estado de la respuesta
+        console.log("STATUS de la ruta categorias:", response.status);// Vemos el estado de la respuesta
         const data = await response.json();
         res.json(data);
     } catch (error) {
-        console.error("Error al obtener datos de las categorias:", error)
+        console.error("Error al obtener datos de las categorias:", error);
         res.status(500).json({error: "Error al obtener datos"});
     }
 });
