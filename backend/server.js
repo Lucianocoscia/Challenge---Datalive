fcc28dcae113f84591e668db6d7586e516b01a83
Luciano <lucianocoscia5@gmail.com>
2025-07-18 06:38:13 -0300

    Agregado endpoint /categorias y su logica correspondiente

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,33 @@ app.get("/api/cryptos", async(req, res) => {
                 'Content-Type': 'application/json',
             }
         });
-        console.log("STATUS:", response.status) // de esta forma veo el estado de la respuesta
+        console.log("STATUS:", response.status) // Vemos el estado de la respuesta
         const data = await response.json();
         res.json(data);
     } catch (error) {
-        console.error("Error al obtener datos:", error)
+        console.error("Error al obtener datos del listado normal:", error)
         res.status(500).json({error: "Error al obtener datos"});
     }
 });
+app.get("/api/cryptos/categorias", async(req, res) => {
+    try {
+        const response = await fetch(
+            url+"/v1/cryptocurrency/categories?limit=100", {
+            method: "GET",
+            headers: {
+                "X-CMC_PRO_API_KEY": apiKey,
+                'Content-Type': 'application/json',
+            }
+        });
+        console.log("STATUS de la ruta categorias:", response.status) // Vemos el estado de la respuesta
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error("Error al obtener datos de las categorias:", error)
+        res.status(500).json({error: "Error al obtener datos"});
+    }
+});
+
 
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en puerto ${PORT}`)
